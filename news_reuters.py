598a3a442b9279c2a1ae175aa41603e598de7a14
PurpleDin0 <57679473+PurpleDin0@users.noreply.github.com>
2020-05-18 14:46:38 -0400

    Update news_reuters.py

--- news_reuters.py
+++ news_reuters.py
@@ -2,7 +2,15 @@
 """
 Created on Sat Jul 13 15:23:39 2019
 
-@author: Dr. Mark M. Bailey | National Intelligence University
+@author: Dr. Mark M. Bailey | National Intelligence 
+# Changelog 18 May 2020 by team Bad Ozone Grasshoppers
+- 1. main() updated to take two options browser agent and news_dump_filename
+-- 1.2. browser agents supported are 'Firefox' and 'Chrome'
+- 2. Updated to use pandas to read and save JSON files instead of pickle files 
+-    This was done to reduce code complexity and increase security (below are updates)
+-- 2.1. save_pickle() replaced with pd.DataFrame.to_json()
+-- 2.2. open_pickle() replaced with pd.DataFrame.read_json()
+-- 2.3. concat_lists() replaced with pd.concat()
 """
 
 #News Scrape
@@ -13,7 +21,9 @@ import time
 from bs4 import BeautifulSoup
 import requests
 from lxml import html
-import pickle
+#import pickle # no longer needed in json functions replaced pickle
+#import json # no longer needed pandas library simplified codebase
+import pandas as pd
 import os
 
 #Define functions
@@ -21,29 +31,40 @@ import os
 GENERAL FUNCTIONS
 """
 
+#save object to JSON 
+#def save_json(output_path, json_object, file_name):
+#    output_name = os.path.join(output_path, file_name + '.json')
+#    with open(output_name, 'w') as json_file:
+#        json.dump(json_object, json_file)
+        
+#open JSON file
+#def open_json(json_path):
+#    with open(json_path, 'rb') as json_file:
+#        object_name = json.load(json_file)
+#    return object_name
+        
 #save object to pickle
-def save_pickle(output_path, pickle_object, file_name):
-    output_name = os.path.join(output_path, file_name + '.pkl')
-    with open(output_name, 'wb') as pkl_object:
-        pickle.dump(pickle_object, pkl_object)
+#Deprecated replaced by save_json function
+#def save_pickle(output_path, pickle_object, file_name):
+#    output_name = os.path.join(output_path, file_name + '.pkl')
+#    with open(output_name, 'wb') as pkl_object:
+#        pickle.dump(pickle_object, pkl_object)
 
 #open pickle file
-def open_pickle(pickle_path):
-    with open(pickle_path, 'rb') as pickle_file:
-        object_name = pickle.load(pickle_file)
-    return object_name
+#Deprecated by open_json function
+#def open_pickle(pickle_path):
+#    with open(pickle_path, 'rb') as pickle_file:
+#        object_name = pickle.load(pickle_file)
+#    return object_name
 
 #Get urls from news object
-def open_file(news_object_path):
-    news_object_file = os.path.join(news_object_path, 'news_dump_object.pkl')
-    old_news = open_pickle(news_object_file)
-    old_url_list = []
-    for item in old_news:
-        url = item['url']
-        old_url_list.append(url)
-    old_url_set = set(old_url_list)
-    return old_news, old_url_set
-    
+#updated to use pandas dataframe to parse the URLs and store the set
+def open_file(news_object_path, news_object_file):
+    news_object_file = os.path.join(news_object_path, news_object_file)
+    old_news_df = pd.read_json(news_object_file)
+    old_url_set = set(old_news_df['url'].values)
+    return old_news_df, old_url_set
+
 #url check
 def url_check(old_url_set, url):
     url_set = set([url])
@@ -55,23 +76,34 @@ def url_check(old_url_set, url):
     return check
 
 #Concat outut lists
-def concat_lists(out_lists):
-    output = []
-    for outlist in out_lists:
-        output = output + outlist
-    return output
+#Deprecated now using pandas concat function.
+#def concat_lists(out_lists):
+#    output = []
+#    for outlist in out_lists:
+#        output = output + outlist
+#    return output
 
 """
 GET LINKS FROM HTML
 """
 
 #get HTML with page scroll
-def get_html_scroll(url):
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+#Function updated to allow user to define the browser agent 
+#Browser flag allows the userpa to define the type of browser used by selenium
+#Code supports Firefox and Chrome, default is Firefox if no browser agent is specified
+def get_html_scroll(url, browser_agent = "Firefox"):
+    # calls the webdriver based on the user's selection
+    if browser_agent == "Firefox":
+        browser = webdriver.Firefox()
+    elif browser_agent == "Chrome":
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
+    #else:
+        #else section intentionally empty and reserved for future use
+        
     browser.get(url)
     lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
     match=False
@@ -173,11 +205,11 @@ def get_reuters_elements(soup_list, articles):
             continue
     return out_list
 
-#Execute Reuters script
-def reuters(old_url_set):
+#Execute Reuters scriptdr
+def reuters(old_url_set, browser_agent):
     print('Getting Reuters articles...')
     url = 'https://www.reuters.com/theWire'
-    post_elms = get_html_scroll(url)
+    post_elms = get_html_scroll(url, browser_agent)
     soup = get_soup(post_elms)
     links = get_soup_links(soup)
     articles = get_articles_reuters(links, old_url_set)
@@ -185,18 +217,31 @@ def reuters(old_url_set):
     reuters_list = get_reuters_elements(soup_list, articles)
     return reuters_list
 
-"""
+"""python
 MAIN SCRIPT
 """
-def main():
+#updated function to allow user to specify a browser agent and news_dump_object filename
+#If not specified the code will default to  Firefox and news_dump_object.json
+def main(browser_agent ="Firefox", news_object_file ='news_dump_object.json'):
+    #Check if the requested browser agent is Firefox or Chrome
+    #If no agent is passed code will defualt to Firefox 
+    if browser_agent == "Chrome" or browser_agent == "Firefox":
+       print("Executing using", browser_agent, "webdriver")
+    else:
+        print(browser_agent, " is not a recognized browser agent, please use 'Firefox' or 'Chrome'")
+        exit(1)
     news_object_path = os.getcwd()
     output_path = os.getcwd()
-    old_news, old_url_set = open_file(news_object_path)
-    reuters_list = reuters(old_url_set)
-    out_lists = [old_news, reuters_list]
-    output_reuters = concat_lists(out_lists)
+    #updated to allow user to specify a news_object_filename
+    old_news_df, old_url_set = open_file(news_object_path, news_object_file)
+    reuters_list_df = pd.DataFrame(reuters(old_url_set, browser_agent))
+    #Updated to use pandas concat function
+    output_reuters_df = pd.concat([old_news_df, reuters_list_df], ignore_index=True)
     print('Saving news object...')
-    save_pickle(output_path, output_reuters, 'news_dump_object')
+    output_file = os.path.join(output_path, news_object_file)
+    #Save output to JSON file using pandas
+    output_reuters_df.to_json(path_or_buf=output_file)
+    output_reuters = output_reuters_df.to_json()
     return output_reuters
 
 
