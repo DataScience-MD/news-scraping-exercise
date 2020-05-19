bd65ff5df8276c4713fa4e6b484b8310087b6690
JeffPier <61338243+jeffpier@users.noreply.github.com>
2020-05-19 02:41:18 -0400

    Changed a lot.
    Added support for local chrome install, cleanup, banner, fixed deprecated options calls for chrome and selenium, added URL scheme validation, reformatted file imports, methods, and lonf strings.

--- news_reuters.py
+++ news_reuters.py
@@ -17,71 +17,84 @@ Created on Sat Jul 13 15:23:39 2019
 - 3. Updated get_soup_links() function to remove duplicate links from list prior to returning list
 """
 
-#News Scrape
+# News Scrape
 
-#Import libraries
+# Import libraries
 import selenium.webdriver as webdriver
 import time
-from bs4 import BeautifulSoup
 import requests
-from lxml import html
-#import pickle # no longer needed in json functions replaced pickle
-#import json # no longer needed pandas library simplified codebase
 import pandas as pd
 import os
+import urllib.request
+from urllib.parse import urlparse
+import zipfile
+import platform
+import base64
+# import pickle # no longer needed in json functions replaced pickle
+# import json # no longer needed pandas library simplified codebase
 
-#Define functions
+# Import methods
+from selenium.webdriver.chrome.options import Options
+from lxml import html
+from bs4 import BeautifulSoup
+
+
+# Define functions
 """
 GENERAL FUNCTIONS
 """
 
-#save object to JSON 
-#def save_json(output_path, json_object, file_name):
+
+# save object to JSON
+# def save_json(output_path, json_object, file_name):
 #    output_name = os.path.join(output_path, file_name + '.json')
 #    with open(output_name, 'w') as json_file:
 #        json.dump(json_object, json_file)
-        
-#open JSON file
-#def open_json(json_path):
+
+# open JSON file
+# def open_json(json_path):
 #    with open(json_path, 'rb') as json_file:
 #        object_name = json.load(json_file)
 #    return object_name
-        
-#save object to pickle
-#Deprecated replaced by save_json function
-#def save_pickle(output_path, pickle_object, file_name):
+
+# save object to pickle
+# Deprecated replaced by save_json function
+# def save_pickle(output_path, pickle_object, file_name):
 #    output_name = os.path.join(output_path, file_name + '.pkl')
 #    with open(output_name, 'wb') as pkl_object:
 #        pickle.dump(pickle_object, pkl_object)
 
-#open pickle file
-#Deprecated by open_json function
-#def open_pickle(pickle_path):
+# open pickle file
+# Deprecated by open_json function
+# def open_pickle(pickle_path):
 #    with open(pickle_path, 'rb') as pickle_file:
 #        object_name = pickle.load(pickle_file)
 #    return object_name
 
-#Get urls from news object
-#updated to use pandas dataframe to parse the URLs and store the set
+# Get urls from news object
+# updated to use pandas dataframe to parse the URLs and store the set
 def open_file(news_object_path, news_object_file):
     news_object_file = os.path.join(news_object_path, news_object_file)
     old_news_df = pd.read_json(news_object_file)
     old_url_set = set(old_news_df['url'].values)
     return old_news_df, old_url_set
 
-#url check
+
+# url check
 def url_check(old_url_set, url):
-    url_set = set([url])
+    scheme = urlparse(url) # parses the url into parts
+    url_set = {url}
     test_set = old_url_set & url_set
-    if len(test_set) == 0:
+    if len(test_set) == 0 and scheme.scheme: # Fixed parse error added scheme validation, will not process urls without scheme i.e. url missin 'https://'
         check = False
     else:
         check = True
     return check
 
-#Concat outut lists
-#Deprecated now using pandas concat function.
-#def concat_lists(out_lists):
+
+# Concat outut lists
+# Deprecated now using pandas concat function.
+# def concat_lists(out_lists):
 #    output = []
 #    for outlist in out_lists:
 #        output = output + outlist
@@ -91,76 +104,88 @@ def url_check(old_url_set, url):
 GET LINKS FROM HTML
 """
 
-#get HTML with page scroll
-#Function updated to allow user to define the browser agent 
-#Browser flag allows the userpa to define the type of browser used by selenium
-#Code supports Firefox and Chrome, default is Firefox if no browser agent is specified
-def get_html_scroll(url, browser_agent = "Firefox"):
+
+# get HTML with page scroll
+# Function updated to allow user to define the browser agent
+# Browser flag allows the userpa to define the type of browser used by selenium
+# Code supports Firefox and Chrome, default is Firefox if no browser agent is specified
+def get_html_scroll(url, browser_agent="Firefox"):
     # calls the webdriver based on the user's selection
     if browser_agent == "Firefox":
         browser = webdriver.Firefox()
     elif browser_agent == "Chrome":
-        chrome_options = webdriver.ChromeOptions()
+        chrome_options = Options() # Using Options() to fix deprecation warning of manual options declarations
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        browser = webdriver.Chrome('chromedriver', chrome_options=chrome_options)
-    #else:
-        #else section intentionally empty and reserved for future use
-        
+        browser = webdriver.Chrome('chromedriver', options=chrome_options) # Using 'options=' to fix deprecation warning
+    # else:
+    # else section intentionally empty and reserved for future use
+
     browser.get(url)
-    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    match=False
-    while(match==False):
+    lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);" +
+                                       "var lenOfPage=document.body.scrollHeight;" +
+                                       "return lenOfPage;")
+    match = False
+    while (match == False):
         lastCount = lenOfPage
         time.sleep(4)
-        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        if lastCount==lenOfPage:
+        lenOfPage = browser.execute_script("window.scrollTo(0, document.body.scrollHeight);" +
+                                           "var lenOfPage=document.body.scrollHeight;" +
+                                           "return lenOfPage;")
+        if lastCount == lenOfPage:
             time.sleep(4)
-            match=True
+            match = True
     post_elms = browser.page_source
     return post_elms
 
-#get HTML file
+
+# get HTML file
 def get_html(url):
     page = requests.get(url)
     html_out = html.fromstring(page.content)
     text = page.text
     return html_out, text
 
-#get soup
+
+# get soup
 def get_soup(html_string):
     soup = BeautifulSoup(html_string, 'html.parser')
     return soup
 
-#get links
+
+# get links
 def get_soup_links(soup):
     links = []
     for link in soup.find_all('a'):
         out_link = link.get('href')
         links.append(out_link)
-    #Remove duplicates from links
-    #Below line removes all duplicate links prior to passing the list of links back
-    #This is accomplished by converting the list of links into a dictionary and then back to a list
+    # Remove duplicates from links
+    # Below line removes all duplicate links prior to passing the list of links back
+    # This is accomplished by converting the list of links into a dictionary and then back to a list
     links = list(dict.fromkeys(links))
     return links
+
 
 """
 REUTERS FUNCTIONS
 """
-#get article links
+
+
+# get article links
 def get_articles_reuters(links, old_url_set):
     articles = []
     for link in links:
         try:
             if 'article' in link:
-                if url_check(old_url_set, link) == False:
+                if not url_check(old_url_set, link):
                     articles.append(link)
         except:
             continue
     return articles
 
-#get article html
+
+# get article html
 def get_html_reuters(articles):
     soup_list = []
     for article in articles:
@@ -169,9 +194,11 @@ def get_html_reuters(articles):
         soup_list.append(soup)
     return soup_list
 
-#date formatter
+
+# date formatter
 def format_date(date):
-    date_dict = {'January':'1','February':'2','March':'3','April':'4','May':'5','June':'6','July':'7','August':'8','September':'9','October':'10','November':'11','December':'12'}
+    date_dict = {'January': '1', 'February': '2', 'March': '3', 'April': '4', 'May': '5', 'June': '6', 'July': '7',
+                 'August': '8', 'September': '9', 'October': '10', 'November': '11', 'December': '12'}
     split_date = date.split(' ')
     year = split_date[2]
     day_list = split_date[1].split(',')[0]
@@ -180,7 +207,8 @@ def format_date(date):
     out_date = year + '-' + month + '-' + day
     return out_date
 
-#get elements
+
+# get elements
 def get_reuters_elements(soup_list, articles):
     out_list = []
     i = 0
@@ -206,14 +234,16 @@ def get_reuters_elements(soup_list, articles):
                 for p in p_list:
                     article_p.append(p.text)
             out_text = ' '.join(article_p)
-            out_dict = dict([('date',date),('time',a_time),('source','www.reuters.com'),('Title',headline),('Text',out_text),('url',link)])
+            out_dict = dict([('date', date), ('time', a_time), ('source', 'www.reuters.com'), ('Title', headline),
+                             ('Text', out_text), ('url', link)])
             out_list.append(out_dict)
         except:
             print('Unable to decode...skipping article...')
             continue
     return out_list
 
-#Execute Reuters scriptdr
+
+# Execute Reuters script
 def reuters(old_url_set, browser_agent):
     print('Getting Reuters articles...')
     url = 'https://www.reuters.com/theWire'
@@ -225,32 +255,86 @@ def reuters(old_url_set, browser_agent):
     reuters_list = get_reuters_elements(soup_list, articles)
     return reuters_list
 
+
+# Get Chrome
+# Adds local support for chrome driver if not found. function in OS aware, and fetches the appropiate version of
+# chrome driver for the OS, extracts the zip, places the executable in the current working directory. This is the
+# first checked PATH option for execution flow, then /bin, then /sbin, etc. Local placement allows for simpler
+# envirment set-up, and removes the requirement to force a non standard PATH variable into sys.
+def check_chrome():
+    os_platform = platform.system()
+
+    try:
+        if os_platform == 'Windows':
+            open('chromedriver.exe')
+        else:
+            open('chromedriver')
+    except Exception:
+        dl_url = ''
+        print("Chrome Driver not found...installing locally...")
+
+        if os_platform == 'Windows':
+            dl_url = 'https://chromedriver.storage.googleapis.com/81.0.4044.138/chromedriver_win32.zip'
+        else:
+            dl_url = 'https://chromedriver.storage.googleapis.com/81.0.4044.138/chromedriver_linux64.zip'
+
+        with urllib.request.urlopen(dl_url) as dl_file:
+            with open('chromedriver.zip', 'wb') as out_file:
+                out_file.write(dl_file.read())
+                out_file.close()
+        with zipfile.ZipFile('chromedriver.zip', 'r') as zip_file:
+            zip_file.extractall()
+            zip_file.close()
+        os.remove('chromedriver.zip')
+        pass
+
+# Useless but fun
+def banner():
+    print(base64.b64decode("PT09PT09PT09PT09PT09PT09PT09PT09PT09PT09CnwgICBCYWQgT3pvbmUgR3" +
+                           "Jhc3Nob3BwZXJzICAgfAo9PT09PT09PT09PT09PT09PT09PT09PT09PT09PT0=").decode('utf8'))
+
+# Cleanup of temporary files
+def cleanup():
+    if open('chromedriver.exe'):
+        os.remove('chromedriver.exe')
+    elif open('chromedriver'):
+        os.remove('chromedriver')
+    elif open('geckodriver.log'):
+        os.remove('geckodriver.log')
+    return
+
+
 """python
 MAIN SCRIPT
 Updated function to allow user to specify a browser agent and news_dump_object filename
 If not specified the code will default to a browser agent of Firefox and filename of news_dump_object.json
 """
 
-def main(browser_agent ="Firefox", news_object_file ='news_dump_object.json'):
-    #Check if the requested browser agent is Firefox or Chrome
-    #If no agent is passed code will defualt to Firefox 
+
+def main(browser_agent="Chrome", news_object_file='news_dump_object.json'):
+    banner()
+    # Check if the requested browser agent is Firefox or Chrome
+    # If no agent is passed code will defualt to Firefox
     if browser_agent == "Chrome" or browser_agent == "Firefox":
-       print("Executing using", browser_agent, "webdriver")
+        print("Executing using", browser_agent, "webdriver")
+        if browser_agent == "Chrome":
+            check_chrome()
     else:
         print(browser_agent, " is not a recognized browser agent, please use 'Firefox' or 'Chrome'")
         exit(1)
     news_object_path = os.getcwd()
     output_path = os.getcwd()
-    #updated below line of code to allow user to specify a news_object_filename
-    old_news_df, old_url_set = open_file(news_object_path, news_object_file)
+    # updated below line of code to allow user to specify a news_object_filename
+    old_news_df, old_url_set = open_file(news_object_path, news_object_file) # *** What if this is first run and there is no json? ***
     reuters_list_df = pd.DataFrame(reuters(old_url_set, browser_agent))
-    #Updated to use pandas concat function
+    # Updated to use pandas concat function
     output_reuters_df = pd.concat([old_news_df, reuters_list_df], ignore_index=True)
     print('Saving news object...')
     output_file = os.path.join(output_path, news_object_file)
-    #Save output to JSON file using pandas
+    # Save output to JSON file using pandas
     output_reuters_df.to_json(path_or_buf=output_file)
     output_reuters = output_reuters_df.to_json()
+    cleanup()
     return output_reuters
 
 
